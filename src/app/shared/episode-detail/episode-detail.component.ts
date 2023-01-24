7648b264891139b92922eff6f8e0e7b46884e603
Leidy marin <leidymarin599@gmail.com>
2023-01-23 22:32:13 -0500

    repo

---
+++ src/app/shared/episode-detail/episode-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-episode-detail',
+  templateUrl: './episode-detail.component.html',
+  styleUrls: ['./episode-detail.component.scss'],
+})
+export class EpisodeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
